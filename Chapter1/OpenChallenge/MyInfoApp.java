569e86b52e4ca41054aabe61fb00d0feadd03fe7
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-05-28 00:26:35 +0000

    OpenChallenge

---
+++ Chapter1/OpenChallenge/MyInfoApp.java
@@ -0,0 +1,7 @@
+public class MyInfoApp {
+	public static void main(String[] args) {
+		System.out.println("Kim SangJae");
+		System.out.println("23 years old");
+		System.out.println("Deartment of Computer Engineering");
+	}
+}
