dead25227f7acbc7a50f8289431f5bd806bdad7b
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-06-24 00:13:08 +0000

    Practical question 9)

---
+++ Chapter3/PrintRandomValueArray.java
@@ -0,0 +1,22 @@
+public class PrintRandomValueArray {
+	public static void main(String[] args) {
+		int num[][] = new int[4][4];
+		
+		for (int i = 0; i<num.length; i++)
+		{
+			for(int j = 0; j<num[i].length; j++)
+			{
+				num[i][j] = (int)(Math.random()*10+1);
+			}
+		}
+		
+		for(int i = 0; i<num.length; i++)
+		{
+			for(int j =0; j<num[i].length; j++)
+			{
+				System.out.print(num[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
