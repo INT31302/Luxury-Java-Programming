355a80025b6e01ce858dc476c8ffb3d76ed52c5e
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-06-27 01:42:14 +0000

    Practical question 12)

---
+++ Chapter3/PrintSubjectsScore.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class PrintSubjectsScore {
+	public static void main(String[] args) {
+		String course[] = {"Java", "C++", "HTML5", "컴퓨터구조", "안드로이드"};
+		int score [] = {95, 88, 76, 62, 55};
+		Scanner s = new Scanner(System.in);
+		String input_name;
+		int cnt = -1;
+		while(true)
+		{
+			System.out.print("과목 이름>>");
+			input_name = s.next();
+			if(input_name.equals("그만")) break;
+			for(int i =0; i<course.length; i++)
+			{
+				if(input_name.equals(course[i]))
+				{
+					cnt = i;
+					break;
+				}
+			}
+			if(cnt != -1)
+			{
+				System.out.println(course[cnt]+"의 점수는 " + score[cnt]);
+				cnt = -1;
+			}
+			else
+				System.out.println("없는 과목입니다.");
+		}
+		s.close();
+	}
+}
