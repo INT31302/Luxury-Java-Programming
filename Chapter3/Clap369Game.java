c10cdbcd6d7ba6961ae44a276351d40c9fd26dcf
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-07-08 03:02:36 +0000

    Practical question 13)

--- Chapter3/Clap369Game.java
+++ Chapter3/Clap369Game.java
@@ -1,29 +1,20 @@
 public class Clap369Game {
 	public static void main(String[] args) {
-		
 		int num1, num2;
-		for(int i =1; i<100; i++)
-		{
-			if(i<10)
-			{
-				if(i%3==0)
-				{
-					System.out.println(i +"박수 짝");
-				}
+		for(int i =1; i<100; i++){
+			if(i<10){
+				if(i%3==0)System.out.println(i +"박수 짝");
 			}
-			else
-			{
+			else{
 				num1 = i / 10;
 				num2 = i % 10;
-				if((num1 % 3 ==0 || num2 % 3 ==0)&& num2 !=0 )
-				{
+				if((num1 % 3 ==0 || num2 % 3 ==0)&& num2 !=0 ){
 					System.out.print(i + "박수 ");
 					if(num1 % 3 == 0) System.out.print("짝");
 					if(num2 % 3 ==0) System.out.print("짝");
 					System.out.println();
 				}
 			}
-			
 		}
 	}
 }
