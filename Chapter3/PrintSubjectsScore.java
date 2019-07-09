3377eb21d2b7988616d0e01d21011fd722bc1181
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-07-09 00:40:36 +0000

    Practical question 14)

--- Chapter3/PrintSubjectsScore.java
+++ Chapter3/PrintSubjectsScore.java
@@ -7,21 +7,17 @@ public class PrintSubjectsScore {
 		Scanner s = new Scanner(System.in);
 		String input_name;
 		int cnt = -1;
-		while(true)
-		{
+		while(true){
 			System.out.print("과목 이름>>");
 			input_name = s.next();
 			if(input_name.equals("그만")) break;
-			for(int i =0; i<course.length; i++)
-			{
-				if(input_name.equals(course[i]))
-				{
+			for(int i =0; i<course.length; i++){
+				if(input_name.equals(course[i])){
 					cnt = i;
 					break;
 				}
 			}
-			if(cnt != -1)
-			{
+			if(cnt != -1){
 				System.out.println(course[cnt]+"의 점수는 " + score[cnt]);
 				cnt = -1;
 			}
