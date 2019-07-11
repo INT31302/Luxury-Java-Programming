a6d84f23fe085c551b836796917791f957b3f75e
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-07-11 00:10:14 +0000

    Practical question 16)

--- Chapter3/HandGame.java
+++ Chapter3/HandGame.java
@@ -14,29 +14,23 @@ public class HandGame {
 		{
 		player = s.next();
 		s.close();
-		if(player.equals("그만"))
-		{
+		if(player.equals("그만")){
 			System.out.println("게임을 종료합니다.");
 			return;
 		}
 		com = (int)(Math.random()*3);
-		for(int i=0; i<str.length; i++)
-		{
+		for(int i=0; i<str.length; i++){
 			if(player.equals(str[i]))
 				plr = i;
 		}
 		System.out.print("사용자 = " + str[plr] + ", 컴퓨터 = "+str[com] + ", ");
-		if(plr == com)
-		{
+		if(plr == com){
 			System.out.println("비겼습니다.");
 		}
-		else
-		{
-		switch(plr)
-		{
+		else{
+		switch(plr){
 			case 0: // 플레이어 가위
-				switch(com)
-				{
+				switch(com){
 				case 1: // 컴 바위
 					System.out.println("컴퓨터가 이겼습니다.");
 					break;
@@ -46,8 +40,7 @@ public class HandGame {
 				}
 			break;
 			case 1: //플레이어 바위
-				switch(com)
-				{
+				switch(com){
 				case 0: // 컴 가위
 					System.out.println("사용자가 이겼습니다.");
 					break;
@@ -57,8 +50,7 @@ public class HandGame {
 				}
 			break;
 			case 2: //플레이어 보
-				switch(com)
-				{
+				switch(com){
 				case 0: // 컴 가위
 					System.out.println("컴퓨터가 이겼습니다.");
 					break;
@@ -70,8 +62,7 @@ public class HandGame {
 			}
 		}
 	}
-		catch(ArrayIndexOutOfBoundsException e)
-		{return;}
+		catch(ArrayIndexOutOfBoundsException e){return;}
 	}
 	}
 }
