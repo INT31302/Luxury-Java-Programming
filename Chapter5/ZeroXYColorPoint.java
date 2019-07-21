1bd9e055df44e54482534484576084e973017a97
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-07-21 14:00:30 +0900

    Practical question 6)

---
+++ Chapter5/ZeroXYColorPoint.java
@@ -0,0 +1,35 @@
+class Point{
+	private int x, y;
+	public Point(int x, int y) {this.x = x; this.y = y;}
+	public int getX() {return x;}
+	public int getY() {return y;}
+	protected void move(int x, int y) {this.x = x; this.y = y;}
+}
+
+class ColorPoint extends Point{
+	private String color;
+	public ColorPoint(){
+		super(0,0);
+		color = "Black";
+	}
+	public ColorPoint(int x, int y){
+		super(x,y);
+		color ="Black";
+	}
+	
+	public void setXY(int x, int y) {move(x,y);}
+	public void setColor(String color) {this.color = color;}
+	public String toString() {return color+"색의 ("+getX()+","+getY()+")의 점";}
+	
+}
+public class ZeroXYColorPoint {
+	public static void main(String[] args) {
+		ColorPoint zeroPoint = new ColorPoint();
+		System.out.println(zeroPoint.toString() + "입니다.");
+		
+		ColorPoint cp = new ColorPoint(10, 10);
+		cp.setXY(5,5);
+		cp.setColor("RED");
+		System.out.println(cp.toString()+"입니다.");
+	}
+}
