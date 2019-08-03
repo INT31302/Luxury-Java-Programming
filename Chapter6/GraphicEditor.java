74d95a8cab17683ebdccd45f74f42b5f87d7ece6
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-08-03 23:40:13 +0900

    Practical question 4)

---
+++ Chapter6/GraphicEditor.java
@@ -0,0 +1,10 @@
+package _4_app;
+import _4_base.*;
+import _4_derived.*;
+
+public class GraphicEditor {
+	public static void main(String[] args) {
+		Shape shape = new Circle();
+		shape.draw();
+	}
+}
