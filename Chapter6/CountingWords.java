37f6d940764b0f36bac3bf00244b056205df9410
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2019-08-07 00:09:17 +0000

    Practical question 7)

---
+++ Chapter6/CountingWords.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CountingWords {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		StringTokenizer st;
+		String Words;
+		int cnt;
+		while(true)
+		{
+			System.out.print(">>");
+			Words = s.nextLine();
+			if(Words.equals("그만")) {
+				System.out.println("종료합니다...");
+				s.close();
+				return;
+			}
+			st = new StringTokenizer(Words, " ");
+			cnt = st.countTokens();
+			System.out.println("어절 개수는 " + cnt);
+		}
+		
+	}
+}
